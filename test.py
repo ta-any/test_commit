8e1b6d07747b51c633267922c5a3c501ddebe81d
Tany Zhmakova <49059282+ta-any@users.noreply.github.com>
2025-12-17 16:28:18 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ print("start test...")
 import unittest
 print("test_example_unittest.py")
 def add(a: int, b: int) -> int:
-    print("start add()")
     return a + b
 
 class TestMathOperations(unittest.TestCase):
